c33856e1d19683b56881411e81445b5d88fed983
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-11-22 12:01:32 -0800

    add all of alecs intial luigi data infra over

---
+++ data/data/cli.py
@@ -0,0 +1,88 @@
+import logging
+import os
+
+from gwpy.timeseries import TimeSeriesDict
+from jsonargparse import ActionConfigFile, ArgumentParser
+from utils.logging import configure_logging
+
+from data.find import DataQualityDict
+
+
+def fetch(
+    start: float, end: float, channels: list[str], sample_rate: float
+) -> TimeSeriesDict:
+    """
+    Simple wrapper to annotate and simplify
+    the kwargs so that jsonargparse can build
+    a parser out of them.
+    """
+
+    logging.info(
+        "Fetching {}s worth of data starting at GPS timestamp {}".format(
+            end - start, start
+        )
+    )
+    X = TimeSeriesDict.get(start=start, end=end, channels=channels)
+
+    logging.info(f"Data downloaded, resampling to {sample_rate}Hz")
+    return X.resample(sample_rate)
+
+
+def main(args=None):
+    query_parser = ArgumentParser()
+    query_parser.add_method_arguments(DataQualityDict, "query_segments")
+    query_parser.add_argument("--output-file", "-o", type=str)
+
+    fetch_parser = ArgumentParser()
+    fetch_parser.add_function_arguments(fetch)
+    fetch_parser.add_argument("--sample-rate", type=float)
+    fetch_parser.add_argument("--output-directory", "-o", type=str)
+    fetch_parser.add_argument("--prefix", "-p", type=str, default="deepclean")
+
+    parser = ArgumentParser()
+    parser.add_argument("--config", action=ActionConfigFile)
+    parser.add_argument("--log-file", type=str, default=None)
+    parser.add_argument("--verbose", type=bool, default=False)
+
+    subcommands = parser.add_subcommands()
+    subcommands.add_subcommand("query", query_parser)
+    subcommands.add_subcommand("fetch", fetch_parser)
+
+    args = parser.parse_args(args)
+    configure_logging(args.log_file, args.verbose)
+
+    if args.subcommand == "query":
+        args = args.query.as_dict()
+        output_file = args.pop("output_file")
+
+        logging.info(
+            "Querying active segments in interval ({start}, {end})".format(
+                **args
+            )
+        )
+        segments = DataQualityDict.query_segments(**args)
+
+        logging.info(
+            "Discovered {} valid segments, writing to {}".format(
+                len(segments), output_file
+            )
+        )
+        segments.write(output_file)
+    elif args.subcommand == "fetch":
+        args = args.fetch.as_dict()
+        output_directory = args.pop("output_directory")
+        prefix = args.pop("prefix")
+        X = fetch(**args)
+
+        duration = args["end"] - args["start"]
+        fname = "{}-{}-{}.hdf5".format(
+            prefix, int(args["start"]), int(duration)
+        )
+        fname = os.path.join(output_directory, fname)
+
+        logging.info(f"Writing downloaded data to {fname}")
+        X.write(fname, format="hdf5")
+
+
+if __name__ == "__main__":
+    main()
---
+++ yoshi/tasks/base.py
@@ -0,0 +1,103 @@
+import os
+import shlex
+import subprocess
+from pathlib import Path
+from typing import Callable
+
+import law
+import luigi
+from law.contrib import singularity
+
+root = Path(__file__).resolve().parent.parent
+
+
+class YoshiSandbox(singularity.SingularitySandbox):
+    sandbox_type = "yoshi"
+    config_section_prefix = "yoshi"
+
+    def _get_volumes(self):
+        volumes = super()._get_volumes()
+        if self.task and getattr(self.task, "dev", False):
+            volumes[root] = "/opt/yoshi"
+        return volumes
+
+
+# TODO: what is this doing / why is this needed?
+law.config.update(
+    {
+        "yoshi": {
+            "stagein_dir_name": "stagein",
+            "stageout_dir_name": "stageout",
+            "law_executable": "law",
+        },
+        "yoshi_env": {},
+        "yoshi_volumes": {},
+    }
+)
+
+
+class YoshiTask(law.SandboxTask):
+    image = luigi.Parameter()
+    dev = luigi.BoolParameter(default=False)
+    gpus = luigi.Parameter(default="")
+
+    container_root = luigi.Parameter(
+        default=os.getenv("DEEPCLEAN_CONTAINER_ROOT", "")
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not os.path.isabs(self.image):
+            self.image = os.path.join(self.container_root, self.image)
+
+        if not os.path.exists(self.image):
+            raise ValueError(
+                f"Could not find path to container image {self.image}"
+            )
+
+    @property
+    def sandbox(self):
+        return f"singularity::{self.image}"
+
+    @property
+    def singularity_forward_law(self) -> bool:
+        return False
+
+    @property
+    def singularity_allow_binds(self) -> bool:
+        return True
+
+    @property
+    def singularity_args(self) -> Callable:
+        def arg_getter():
+            if self.gpus:
+                return ["--nv"]
+            return []
+
+        return arg_getter
+
+    def sandbox_env(self, _):
+        if self.gpus:
+            return {"CUDA_VISIBLE_DEVICES": self.gpus}
+        return {}
+
+    @property
+    def python(self) -> str:
+        return "/usr/local/bin/python"
+
+    @property
+    def command(self) -> str:
+        return [self.command, "-c", "print('Hello world')"]
+
+    def run(self):
+        try:
+            subprocess.run(
+                self.command, capture_output=True, check=True, text=True
+            )
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(
+                "Command '{}' failed with return code {} "
+                "and stderr:\n{}".format(
+                    shlex.join(e.cmd), e.returncode, e.stderr
+                )
+            ) from None
